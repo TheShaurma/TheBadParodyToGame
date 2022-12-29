d711964bb549811f0213c7bab3630cbaf9fb5128
TheShaurma <vevrton@mail.ru>
2022-12-29 17:02:02 +0700

    Added documentation for new classes

--- TheBadParodyToGame/ObjectsInArea/CannotMoveObjectException.java
+++ TheBadParodyToGame/ObjectsInArea/CannotMoveObjectException.java
@@ -3,12 +3,24 @@ package TheBadParodyToGame.ObjectsInArea;
 import TheBadParodyToGame.area.position.IntegerPosition2D;
 
 // TODO: create tests for CannotMoveObjectException
-// TODO: write documentation for CannotMoveObjectException
+/**
+ * CannotMoveObjectException should be thrown were object manager can't move
+ * object.
+ */
 public class CannotMoveObjectException extends ManageObjectException {
+    /**
+     * @param position where manager can't move object;
+     * @param object   which can't be moved;
+     */
     public CannotMoveObjectException(IntegerPosition2D position, ObjectInArea object) {
         super(position, object);
     }
 
+    /**
+     * Return information in string.
+     * 
+     * @return {@code "Cannot move " + getObject() + " to " + getPosition() + "."}
+     */
     @Override
     public String toString() {
         return "Cannot move " + getObject() + " to " + getPosition() + ".";
--- TheBadParodyToGame/ObjectsInArea/ObjectManager.java
+++ TheBadParodyToGame/ObjectsInArea/ObjectManager.java
@@ -2,16 +2,29 @@ package TheBadParodyToGame.ObjectsInArea;
 
 import TheBadParodyToGame.area.CheckeredArea;
 import TheBadParodyToGame.area.position.BusyPositionException;
+import TheBadParodyToGame.area.position.EmptyPositionException;
 import TheBadParodyToGame.area.position.GameIntegerPosition2D;
 import TheBadParodyToGame.area.position.IntegerPosition2D;
 import TheBadParodyToGame.area.position.PositionException;
 
-// TODO: write documentation fo ObjectManager
+/**
+ * ObjectManager is abstract class with logic to move one object in
+ * CheckeredArea.
+ */
 public abstract class ObjectManager {
     private IntegerPosition2D currentPosition;
     private final CheckeredArea area;
     private final ObjectInArea object;
 
+    /**
+     * Constructor of ObjectManager.
+     * obj should be in area at pos already.
+     *
+     * @param pos
+     * @param area
+     * @param obj
+     * @throws PositionException
+     */
     public ObjectManager(IntegerPosition2D pos, CheckeredArea area, ObjectInArea obj) throws PositionException {
         this.area = area;
         currentPosition = pos;
@@ -20,6 +33,19 @@ public abstract class ObjectManager {
         checkObjectAvailability();
     }
 
+    /**
+     * Move object 1 step up.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX();
+     * int newY = oldPos.getY() + 1;
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectUp() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX();
@@ -29,6 +55,19 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object 1 step down.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX();
+     * int newY = oldPos.getY() - 1;
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectDown() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX();
@@ -38,6 +77,19 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object 1 step left.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX() + 1;
+     * int newY = oldPos.getY();
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectLeft() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX() - 1;
@@ -47,6 +99,19 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object 1 step right.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX() + 1;
+     * int newY = oldPos.getY();
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectRight() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX() + 1;
@@ -56,6 +121,19 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object 1 step up and left.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX() - 1;
+     * int newY = oldPos.getY() + 1;
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectUpLeft() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX() - 1;
@@ -65,6 +143,19 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object 1 step up and right.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX() + 1;
+     * int newY = oldPos.getY() + 1;
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectUpRight() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX() + 1;
@@ -74,6 +165,19 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object 1 step down and left.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX() - 1;
+     * int newY = oldPos.getY() - 1;
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectDownLeft() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX() - 1;
@@ -83,6 +187,19 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object 1 step down and right.
+     * 
+     * Logic of generating of new position:
+     * {@code
+     * int newX = oldPos.getX() + 1;
+     * int newY = oldPos.getY() - 1;
+     * }
+     * 
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectDownRight() throws PositionException {
         IntegerPosition2D oldPos = getCurrentPosition();
         int newX = oldPos.getX() + 1;
@@ -92,6 +209,14 @@ public abstract class ObjectManager {
         moveObjectToPosition(newPos);
     }
 
+    /**
+     * Move object to assigned position in checkered area.
+     * 
+     * @param pos
+     * @throws LostObjectException       if obj isn't in pos;
+     * @throws CannotMoveObjectException if position where object should be after
+     *                                   moving is busy;
+     */
     protected void moveObjectToPosition(IntegerPosition2D newPos) throws PositionException {
         checkObjectAvailability();
 
@@ -107,14 +232,29 @@ public abstract class ObjectManager {
         setCurrentPosition(newPos);
     }
 
+    /**
+     * This method is final.
+     * 
+     * @return Area where object is.
+     */
     protected final CheckeredArea getArea() {
         return area;
     }
 
+    /**
+     * This method is final.
+     * 
+     * @return object where object is.
+     */
     protected final ObjectInArea getObject() {
         return object;
     }
 
+    /**
+     * This method is final.
+     * 
+     * @return position where object is for now.
+     */
     protected final IntegerPosition2D getCurrentPosition() {
         return currentPosition;
     }
@@ -123,7 +263,7 @@ public abstract class ObjectManager {
         currentPosition = pos;
     }
 
-    private void checkObjectAvailability() throws LostObjectException {
+    private void checkObjectAvailability() throws PositionException {
         CheckeredArea area = getArea();
         ObjectInArea obj = getObject();
         IntegerPosition2D pos = getCurrentPosition();
@@ -131,7 +271,7 @@ public abstract class ObjectManager {
 
         try {
             objInPos = area.get(pos);
-        } catch (PositionException e) {
+        } catch (EmptyPositionException e) {
             throw new LostObjectException(e.getPosition(), obj);
         }
 
