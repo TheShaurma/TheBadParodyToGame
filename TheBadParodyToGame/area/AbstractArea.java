18e8361864cef7a8a4f0eb3f55161de4fb040bb7
TheShaurma <vevrton@mail.ru>
2023-10-17 15:09:21 +0700

    Adapted AbstractArea to HashMap

--- TheBadParodyToGame/area/AbstractArea.java
+++ TheBadParodyToGame/area/AbstractArea.java
@@ -13,21 +13,28 @@ import TheBadParodyToGame.area.position.PositionCannotExistInAreaException;
  * DIDN'T adapted for multi-thread using.
  */
 public abstract class AbstractArea<OBJ extends ObjectInArea> implements Area<OBJ> {
-    private AreaItself areaItself = new AreaItself();
+    private final HashMap<IntegerPosition2D, OBJ> areaItself;
+
+    public AbstractArea() {
+        areaItself = new HashMap<>();
+    }
 
     @Override
     public OBJ get(IntegerPosition2D pos) throws EmptyPositionException, PositionCannotExistInAreaException {
+        if (positionIsEmpty(pos)) {
+            throw new EmptyPositionException(pos);
+        }
         return areaItself.get(pos);
     }
 
     @Override
     public void set(IntegerPosition2D pos, OBJ obj) throws PositionCannotExistInAreaException {
-        areaItself.set(pos, obj);
+        areaItself.put(pos, obj);
     }
 
     @Override
     public void place(IntegerPosition2D pos, OBJ obj) throws BusyPositionException, PositionCannotExistInAreaException {
-        if (areaItself.posIsBusy(pos)) {
+        if (positionIsBusy(pos)) {
             throw new BusyPositionException(pos);
         }
 
@@ -65,13 +72,13 @@ public abstract class AbstractArea<OBJ extends ObjectInArea> implements Area<OBJ
             throw new EmptyPositionException(pos);
         }
 
-        areaItself.del(pos);
+        areaItself.remove(pos);
     }
 
     @Override
     public void tryRemove(IntegerPosition2D pos) throws PositionCannotExistInAreaException {
         if (positionIsBusy(pos)) {
-            areaItself.del(pos);
+            areaItself.remove(pos);
         }
     }
 
@@ -85,24 +92,20 @@ public abstract class AbstractArea<OBJ extends ObjectInArea> implements Area<OBJ
             throw new BusyPositionException(newPos);
         }
 
-        OBJ obj = areaItself.get(oldPos);
-        areaItself.del(oldPos);
-        areaItself.set(newPos, obj);
+        place(newPos, get(oldPos));
+        remove(oldPos);
     }
 
     @Override
     public void tryRelocate(IntegerPosition2D oldPos, IntegerPosition2D newPos)
             throws PositionCannotExistInAreaException {
         if (positionIsBusy(oldPos) && positionIsEmpty(newPos)) {
-            OBJ obj;
             try {
-                obj = areaItself.get(oldPos);
+                tryPlace(newPos, get(oldPos));
+                remove(newPos);
             } catch (EmptyPositionException e) {
                 return;
             }
-
-            areaItself.del(oldPos);
-            areaItself.set(newPos, obj);
         }
     }
 
@@ -113,71 +116,17 @@ public abstract class AbstractArea<OBJ extends ObjectInArea> implements Area<OBJ
             throw new EmptyPositionException(oldPos);
         }
 
-        OBJ obj = areaItself.get(oldPos);
-        areaItself.del(oldPos);
-        areaItself.set(newPos, obj);
+        set(newPos, get(oldPos));
+        remove(oldPos);
     }
 
     @Override
     public boolean positionIsEmpty(IntegerPosition2D pos) throws PositionCannotExistInAreaException {
-        return areaItself.posIsEmpty(pos);
+        return !areaItself.containsKey(pos);
     }
 
     @Override
     public boolean positionIsBusy(IntegerPosition2D pos) throws PositionCannotExistInAreaException {
-        return areaItself.posIsBusy(pos);
-    }
-
-    /**
-     * AreaItself
-     */
-    private class AreaItself {
-        // Use areaItself.get(x).get(y) to get object
-        private HashMap<Integer, HashMap<Integer, OBJ>> areaItself = new HashMap<>();
-
-        public void set(IntegerPosition2D pos, OBJ obj) {
-            int x = pos.getX();
-            int y = pos.getY();
-
-            if (!areaItself.containsKey(x)) {
-                areaItself.put(x, new HashMap<>());
-            }
-
-            areaItself.get(x).put(y, obj);
-        }
-
-        public void del(IntegerPosition2D pos) {
-            areaItself.get(pos.getX()).remove(pos.getY());
-        }
-
-        public OBJ get(IntegerPosition2D pos) throws EmptyPositionException {
-            if (posIsEmpty(pos)) {
-                throw new EmptyPositionException(pos);
-            }
-
-            if (!areaItself.containsKey(pos.getX())) {
-                areaItself.put(pos.getX(), new HashMap<>());
-            }
-
-            return areaItself.get(pos.getX()).get(pos.getY());
-        }
-
-        public boolean posIsEmpty(IntegerPosition2D pos) {
-            return !posIsBusy(pos);
-        }
-
-        public boolean posIsBusy(IntegerPosition2D pos) {
-            int x = pos.getX();
-            int y = pos.getY();
-            if (areaItself.containsKey(x)) {
-                if (areaItself.get(x).containsKey(y)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
+        return areaItself.containsKey(pos);
     }
 }
